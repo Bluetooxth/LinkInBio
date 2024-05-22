6c66c6d215c692eef8b155b822b86db519e099c7
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-05-22 16:44:37 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+      </head>
       <body className={syne.className}>{children}</body>
     </html>
   );
